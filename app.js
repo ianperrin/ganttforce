f9e5023693821b078ba50344fac2f1a7fd1af3cb
Ian <ianperrin@gmail.com>
2016-06-19 23:01:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var stormpath = require('express-stormpath');
 var app = express();
+
+app.use(stormpath.init(app, { }));
 
 app.get('/', function (req, res) {
   console.log('Example app getting root path!');
@@ -9,4 +12,3 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port ' + process.env.PORT || 3000);
 });
-
