489e3b0ceeba747f78d4c35dfc720e293b02b4da
Ian <ianperrin@gmail.com>
2016-06-20 20:25:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 });
 
 app.on('stormpath.ready', function () {
-  app.listen(process.env.PORT || 3000, function () {
+  app.listen(app.get('port'), function () {
     console.log('Example app listening on port ' + process.env.PORT || 3000);
   });
 });
