10b5f05be402941ac60c81ab717c7a7a2694ea5b
Ian <ianperrin@gmail.com>
2016-06-20 21:17:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ app.set('view engine', 'ejs');
 
 app.use(stormpath.init(app, { }));
 
-app.get('/', function (req, res) {
-  console.log('Example app getting root path!');
-  res.send('Hello World!');
+// index page 
+app.get('/', function(req, res) {
+  console.log('Ganttforce app getting root path!');
+  res.render('pages/index');
 });
 
 app.on('stormpath.ready', function () {
   app.listen(app.get('port'), function () {
-    console.log('Example app listening on port ' + app.get('port'));
+    console.log('Ganttforce app listening on port ' + app.get('port'));
   });
 });
